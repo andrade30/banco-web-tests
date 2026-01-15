1ffab002e6e9c0d54e9ed6a7be87a2bc6381b4b6
Alessandro Andrade <andrade.sp27@gmail.com>
2026-01-15 16:41:43 -0400

    Implementando comandos customizáveis para Login, Transferencias e Commons

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,30 +6,15 @@ describe('Login', () => {
   });
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').type(credenciais.valida.usuario)
-      cy.get('#senha').type(credenciais.valida.senha)
-    })
-
-    cy.contains('Entrar')
-      .should('be.visible')
-      .click();
+    cy.fazerLogin();
 
     cy.contains('h4', 'Realizar Transferência')
       .should('be.visible')
   })
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').type(credenciais.invalida.usuario)
-      cy.get('#senha').type(credenciais.invalida.senha)
-    })
+    cy.fazerLogincomCredenciaisInvalidas()
 
-    cy.contains('button', 'Entrar')
-      .should('be.visible')
-      .click();
-
-    cy.get('.toast')
-      .should('have.text', 'Erro no login. Tente novamente.');
+    cy.verificarMensagemNoToast('Erro no login. Tente novamente.');
   });
 })
